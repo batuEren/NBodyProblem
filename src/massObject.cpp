caa1c446c5edfea782b37f48ef216ee0aafa305e
Batu Eren <m.b.eren@student.tudelft.nl>
2025-07-17 00:47:03 +0300

    basic mass obj class

--- src/massObject.cpp
+++ src/massObject.cpp
@@ -1,1 +1,30 @@
 #include "MassObject.h"
+#include <glm/glm.hpp>
+#include <glm/gtx/norm.hpp> // for distance2 (optional, for optimization)
+#include <glm/gtx/vector_angle.hpp>
+
+MassObject::MassObject() = default;
+
+MassObject::MassObject(double mass, glm::vec2 pos)
+    : mass(mass), position(pos) {}
+
+double MassObject::getMass() const {
+    return mass;
+}
+
+glm::vec2 MassObject::getPosition() const {
+    return position;
+}
+
+void MassObject::setMass(double m) {
+    mass = m;
+}
+
+void MassObject::setPosition(const glm::vec2& pos) {
+    position = pos;
+}
+
+
+float MassObject::distanceTo(const MassObject& other) const {
+    return glm::distance(position, other.position);
+}
--- src/massObject.h
+++ src/massObject.h
@@ -4,16 +4,19 @@ class MassObject
 {
 	public:
         MassObject();  // Default constructor
-        MassObject(double mass); // Overloaded constructor
+        MassObject(double mass, glm::vec2 pos); // Overloaded constructor
 
         double getMass() const;
+        glm::vec2 getPosition() const;
 
         void setMass(double m);
+        void setPosition(const glm::vec2& pos);
 
-
+        float distanceTo(const MassObject& other) const;
 
 	private:
 		float mass = 1.0f; // mass in kilograms
+        glm::vec2 position = glm::vec2(0.0f, 0.0f); // in kilometers
 
 };
 
